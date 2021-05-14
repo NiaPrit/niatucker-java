803b36d89fb93df4aea93f10cfbec09d92fa3316
Nia Tucker <ntucker1@kent.edu>
2021-05-14 18:38:54 -0400

    Day5Work

--- module-1/05_Command_Line_Programs/exercise-student/java/src/main/java/com/techelevator/LinearConvert.java
+++ module-1/05_Command_Line_Programs/exercise-student/java/src/main/java/com/techelevator/LinearConvert.java
@@ -1,9 +1,37 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class LinearConvert {
 
 	public static void main(String[] args) {
 
+		System.out.println(" Enter  Length ");
+		Scanner theKeyboard = new Scanner(System.in);
+		String aLine = "";
+		aLine = theKeyboard.nextLine();
+
+		System.out.println(" Is the length Meters or Feet ?");
+		String answer = "";
+		answer = theKeyboard.nextLine();
+
+		if (answer.equals ( "M" )) {
+			double ff = (Integer.parseInt(aLine) * 3.280839);
+			System.out.println(aLine + "M is " + ff + "F");
+		}
+		if (answer.equals("F")) {
+			double mm = (Double.parseDouble(aLine) * 3.2808399);
+			System.out.println(aLine + "F is " + mm + "M");
+
+		}
+
 	}
 
 }
+
+// Ask to enter length
+//Get the length
+//Ask if its Meters or Feet
+//Get the answer
+//Convert the answer
+//Print the Conversions
--- module-1/05_Command_Line_Programs/exercise-student/java/src/main/java/com/techelevator/TempConvert.java
+++ module-1/05_Command_Line_Programs/exercise-student/java/src/main/java/com/techelevator/TempConvert.java
@@ -1,9 +1,39 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class TempConvert {
 
 	public static void main(String[] args) {
 
-	}
 
+		System.out.println(" Please Enter Temperature");
+		Scanner theKeyboard = new Scanner(System.in);
+		String aLine = "";
+		aLine = theKeyboard.nextLine();
+
+		System.out.println(" Is The Temperature Celsius or Fahrenheit ?");
+		String answer = "";
+		answer = theKeyboard.nextLine();
+
+		if (answer.equals ("C" )) {
+			double tf = (Integer.parseInt(aLine) * 9 / 5) + 32;
+			System.out.println(aLine + "C is " + tf + "F");
+		}
+		if (answer.equals("F")) {
+			double tc =(Double.parseDouble(aLine) - 32) * 5 / 9;
+			System.out.println (aLine + "F is " + tc + "C");
+
+
+		}
+	}
 }
+
+
+
+// ask to enter temo
+// get temp from user
+//ask user if C or F
+//get answer from user
+//conver answer
+//print what the conversion is
