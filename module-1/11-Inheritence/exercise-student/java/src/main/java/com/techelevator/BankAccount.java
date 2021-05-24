6b29d2c9f3175936d29afb0a5e46399be3a02364
Nia Tucker <ntucker1@kent.edu>
2021-05-24 16:09:25 -0400

    Progress 11

---
+++ module-1/11-Inheritence/exercise-student/java/src/main/java/com/techelevator/BankAccount.java
@@ -0,0 +1,44 @@
+package com.techelevator;
+
+public class BankAccount {
+ // Attributes
+  private String accountHolderName;
+  private String accountNumber;
+  private int    balance;
+
+  // methods
+    public int deposit(int amountToDeposit) {
+        balance = amountToDeposit + balance;
+        return balance;
+    }
+    public int withdraw(int amountToWithdraw){
+        balance = balance - amountToWithdraw;
+        return balance;
+    }
+
+  //Constructor
+    public BankAccount(String accountHolderName, String accountNumber) {
+        this.accountHolderName = accountHolderName;
+        this.accountNumber = accountNumber;
+        this.balance = 0;
+    }
+
+    public BankAccount(String accountHolderName, String accountNumber, int balance) {
+        this.accountHolderName = accountHolderName;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    //getters
+    public String getAccountHolderName() {
+        return accountHolderName;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+}
