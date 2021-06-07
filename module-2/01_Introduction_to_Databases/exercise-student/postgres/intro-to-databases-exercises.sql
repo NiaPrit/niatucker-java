627e0dd419765c5f09e14dee126120dcab44a549
Nia Tucker <ntucker1@kent.edu>
2021-06-07 19:52:31 -0400

    modue2 exercise 1

--- module-2/01_Introduction_to_Databases/exercise-student/postgres/intro-to-databases-exercises.sql
+++ module-2/01_Introduction_to_Databases/exercise-student/postgres/intro-to-databases-exercises.sql
@@ -4,39 +4,115 @@
 --   GNP is expressed in millions of US Dollars
 --   The value immediately after the problem statement is the expected number of rows that should be returned by the query.
 
--- 1. The name and population of all cities in Ontario, Canada (27 rows)
+-- 1. The name and population of all cities in Ontario, Canada (27 rows)  
+-- ignore information in the problem if the data is not in the table
+
+select name, population  --columns that holds the result. must use commas 1st
+from city                -- 
+where district = 'Ontario'
+;
 
 -- 2. The name and population of all cities in Montana (1 row)
+select name, population
+from city
+where district = 'Montana'
+;
 
 -- 3. The name, form of government, and head of state of all countries in Europe (46 rows)
+select name, governmentform, headofstate  -- columns I want to see in the end result
+from country                              -- table that has the data  I need      
+where continent = 'Europe'                -- pull data SPECIFICALLY from Europe row      
+;
 
 -- 4. The name, population, surface area, and average life expectancy of all countries in Asia (51 rows)
+select name, population, surfacearea, lifeexpectancy --columns I want to see in the end result
+from country                                         --table that has the data I need   
+where continent = 'Asia'                            -- pull data SPECIFICALLY from Asia contries    
+;
 
 -- 5. The name, country code, and population of all cities with a population greater than 8 million people (10 rows)
+select name, countrycode, population
+from city
+where population > 8000000
+;
 
 -- 6. The name, country code, and population of all cities with a population less than one thousand people (11 rows)
+select name, countrycode, population
+from city
+where population < 1000
+;
 
 -- 7. The name, continent, and GNP of all countries with a GNP greater than one trillion dollars (6 rows)
+select name, continent,gnp
+from country
+where gnp > 1000000
+;
 
 -- 8. The name, continent, population, GNP, and average life expectancy of all countries with an average life expectancy greater than 80 years (5 rows)
+select name, continent,population,gnp lifeexpectancy
+from country
+where lifeexpectancy > 80
+;
 
 -- 9. The name and population of all cities in the USA with a population of greater than 1 million people (9 rows)
+select name, population
+from city
+where countrycode = 'USA'
+and population > 1000000
+;
 
 -- 10. The name and population of all cities in China with a population of greater than 1 million people (35 rows)
+select name, population
+from city
+where countrycode = 'CHN'
+and population > 1000000
+;
 
 -- 11. The name and region of all countries in North or South America (51 rows)
+select name, region
+from country
+where continent like '%America%'
+;
 
 -- 12. The name, continent, and head of state of all countries whose form of government is a monarchy (43 rows)
+select name, continent, headofstate
+from country
+where governmentform like '%Monarchy%'
+;
 
 -- 13. The name of all cities in the USA with a population between 1 million and 2 million people (6 rows) 
+select name
+from city
+where countrycode = 'USA'
+and population BETWEEN 1000000 AND 2000000
+;
+
 
 -- 14. The name and region of all countries in North or South America except for countries in the Caribbean (27 rows)
+select name, region
+from country
+where region like '%America%'
+and region != 'Caribbean'
+;
+
 
 -- 15. The name, population, and GNP of all countries with a GNP greater than $1 trillion dollars and a population of less than 100 million people (4 rows)
+select name, population, GNP
+from country
+where GNP > 1000000
+AND population < 100000000
+;
+
 
 -- 16. The name and population of all cities in Texas that have a population of greater than 1 million people (3 rows)
+select name, population
+from city
+where district = 'Texas'
+AND population > 1000000
+;
 
 -- 17. The name and average life expectancy of all countries on the continent of Oceania (28 rows)
+
 
 -- 18. The name and average life expectancy of all countries on the continent of Oceania for which an average life expectancy has been provided (i.e. not equal to null) (20 rows)
 
--- module-2/01_Introduction_to_Databases/lecture-student/student-lecture-Frank.sql
+++ module-2/01_Introduction_to_Databases/lecture-student/student-lecture-Frank.sql
@@ -9,14 +9,16 @@
 --
 -- Basic syntax:
 --
---      SELECT   - columns to include in the result (seperate mutiple column reqeusts with commas)
---      FROM     - table containing rows used in the query 
---      WHERE    - rows to include in the result
---      ORDER BY - sequence of rows in the result
+--      SELECT   - columns to include in the result (seperate mutiple column reqeusts with commas) 1st
+--      FROM     - table containing rows used in the query  2nd 
+--      WHERE    - rows to include in the result 3rd
+--      ORDER BY - sequence of rows in the result last
 --                 without an ORDER BY the sequence of the rows in the result is not predictable
 --                 if the sequence of teh rows in teh result matter - code an ORDER BY
 --
--- WHERE predicates:
+-- WHERE predicates: ( Condition - similar to Java if conditions with many more options)
+
+--  NOT EQUALS is either <> OR !=
 --
 --        =  <>  !=  >  >=  <  <= 
 --        IN(list-of-values)      -- alterative to a series of = OR
@@ -40,38 +42,84 @@
 ------------------------------------------------------------------------------------------------------
 
 -- Selecting the names for all countries
-
+select name -- columns I want to see in the results
+ from country-- table with the colums i want
+ ; -- use a semi-colon to mark the end of an SQL statements in a file
 
 -- Selecting the name and population of all countries
-
+select name, population -- columns to see in the result in the order you want to see them
+ from country           -- table with the columns we want
+ ;
+ select population, name
+ from country
+ ;
 
 -- Selecting all columns from the city table
-
+select *  -- * means from all columns in the order defined 
+from city
+;
 
 -- SELECT ... FROM ... WHERE
 -- Selecting the cities in Ohio
-
+select name             -- show me the name 
+from city               -- from the city table
+where district = 'Ohio' -- only include the rows for district = 'ohio'
+                        -- string literals are coded inside apostrophes ( not double quotes like Java)
+                        -- the case of the string matters MUST match what is in the table
+;
 
 -- Selecting countries that gained independence in the year 1776
-
+select name
+from country
+where indepyear = 1776; --numeric literals are just codes as numbers
+;
 
 -- Selecting countries not in Asia
+select name, continent -- sometimes include columsn not asked for to verify your results
+from country
+where continent <> 'Asia'
+;
 
-
--- Selecting countries that do not have an independence year
+-- Selecting countries that do not have an independence year --independence year is null
+-- in relational data bases null repre a missing or unknown value
+-- since its value is unknow we cannt use normal WHERE predicates to test it ( = != <> < >)
+--special WHERE predicates are used to check nulls - NOT NULL or IS NULL
+select name, indepyear
+from country
+where indepyear is null
+; -- must use the special 
 
 -- Selecting countries that do have an independence year
-
+select name, indepyear
+from country
+where indepyear is not null
+;
 
 -- Selecting countries that have a population greater than 5 million
-
+select name, population
+from country
+where population > 500000
 
 
 -- SELECT ... FROM ... WHERE ... AND/OR
 -- Selecting cities in Ohio and Population greater than 400,000
 
--- Selecting country names on the continent North America or South America
+select name, population, district
+from city
+where population > 400000 -- a full predicate is required on each side or and AND / OR
+and district = 'Ohio'  
+;  -- a full predicate id column operator value
 
+
+
+
+
+-- Selecting country names on the continent North America or South America
+select name, continent
+from country
+where continent = 'North America'
+or     continent = 'South America'
+;
 
 
 
