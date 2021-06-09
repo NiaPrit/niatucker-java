69c3c0a7fbdda38e02364ebcdab01c77bd719ce4
Nia Tucker <ntucker1@kent.edu>
2021-06-08 20:55:19 -0400

    AF exercise

--- module-2/02_Aggregate_Functions_and_GROUP_BY/exercise-student/postgres/aggregate-functions-exercises.sql
+++ module-2/02_Aggregate_Functions_and_GROUP_BY/exercise-student/postgres/aggregate-functions-exercises.sql
@@ -1,4 +1,4 @@
-﻿-- The following queries utilize the "world" database.
+-- The following queries utilize the "world" database.
 -- Write queries for the following problems. 
 -- Notes:
 --   GNP is expressed in units of one million US Dollars
@@ -7,51 +7,117 @@
 
 -- 1. The name and state plus population of all cities in states that border Ohio 
 -- (i.e. cities located in Pennsylvania, West Virginia, Kentucky, Indiana, and 
--- Michigan).  
+-- Michigan). 
 -- The name and state should be returned as a single column called 
--- name_and_state and should contain values such as ‘Detroit, Michigan’.  
+-- name_and_state and should contain values such as Detroit, Michigan.  
 -- The results should be ordered alphabetically by state name and then by city 
 -- name. 
 -- (19 rows)
 
+Select name|| ',' || district as name_and_state, population --concatenate name and district to get it on one line  (combine) 
+From city
+where district = 'Pennsylvania'
+or district = 'West Virginia'
+or district = 'Kentucky'
+or district = 'Indiana'
+or district ='Michigan'
+--group BY name, district, population
+;
+
+
 -- 2. The name, country code, and region of all countries in Africa.  The name and
 -- country code should be returned as a single column named country_and_code 
--- and should contain values such as ‘Angola (AGO)’ 
+-- and should contain values such as Angola (AGO)
 -- (58 rows)
+Select name|| ',' || code as country_and_code, region
+From country
+where continent = 'Africa'
+;
+
 
 -- 3. The per capita GNP (i.e. GNP multipled by 1000000 then divided by population) of all countries in the 
 -- world sorted from highest to lowest. Recall: GNP is express in units of one million US Dollars 
 -- (highest per capita GNP in world: 37459.26)
 
+Select name, (gnp * 1000000) / population AS per_capita_gnp 
+From country 
+where population != 0
+ORDER BY per_capita_gnp DESC;
+
 -- 4. The average life expectancy of countries in South America.
 -- (average life expectancy in South America: 70.9461)
+Select AVG (lifeexpectancy)
+From country
+where continent = 'South America'
+;
+
+
 
 -- 5. The sum of the population of all cities in California.
 -- (total population of all cities in California: 16716706)
+Select SUM (population)
+From city
+where district = 'California'
+;
+
 
 -- 6. The sum of the population of all cities in China.
 -- (total population of all cities in China: 175953614)
 
+Select SUM (population)
+from city
+where countrycode = 'CHN'
+;
+
 -- 7. The maximum population of all countries in the world.
 -- (largest country population in world: 1277558000)
 
+select MAX (population)
+from country
+;
+
 -- 8. The maximum population of all cities in the world.
 -- (largest city population in world: 10500000)
+select MAX (population)
+from city
+;
 
 -- 9. The maximum population of all cities in Australia.
 -- (largest city population in Australia: 3276207)
+select MAX (population)
+from city
+where countrycode = 'AUS'
+;
+
 
 -- 10. The minimum population of all countries in the world.
 -- (smallest_country_population in world: 50)
 
+select min (population)
+from country
+where population != 0 
+;
+
 -- 11. The average population of cities in the United States.
 -- (avgerage city population in USA: 286955.3795)
 
+select AVG (population)
+from city
+where countrycode = 'USA'
+;
+
+
 -- 12. The average population of cities in China.
 -- (average city population in China: 484720.6997 approx.)
+select AVG (population)
+from city
+where countrycode = 'CHN'
+;
+
 
 -- 13. The surface area of each continent ordered from highest to lowest.
 -- (largest continental surface area: 31881000, "Asia")
+
 
 -- 14. The highest population density (population divided by surface area) of all 
 -- countries in the world. 
