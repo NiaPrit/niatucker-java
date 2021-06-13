064a2338dd5cb9baebac67b560735fe8a28671f3
Nia Tucker <ntucker1@kent.edu>
2021-06-12 21:57:25 -0400

    UpdatedInsertUpdateDelete

--- module-2/04_INSERT_UPDATE_and_DELETE/exercise-student/constraints-and-transactions-exercises.sql
+++ module-2/04_INSERT_UPDATE_and_DELETE/exercise-student/constraints-and-transactions-exercises.sql
@@ -20,27 +20,95 @@
 
 
 
-Begin transaction;
+
 
 
 -- 1. Add actors, Hampton Avenue, and Lisa Byway to the actor table.
+BEGIN TRANSACTION;
 
--- 2. Add "Euclidean PI", "The epic story of Euclid as a pizza delivery boy in
--- ancient Greece", to the film table. The movie was released in 2008 in English.
--- Since its an epic, the run length is 3hrs and 18mins. There are no special
--- features, the film speaks for itself, and doesn't need any gimmicks.
+SELECT *
+from actor;
 
--- 3. Hampton Avenue plays Euclid, while Lisa Byway plays his slightly
--- overprotective mother, in the film, "Euclidean PI". Add them to the film.
+insert into actor (first_name, last_name)
+values('HAMPTON','AVENUE');
 
--- 4. Add Mathmagical to the category table.
+insert into actor (first_name, last_name)
+values('LISA','BYWAY');
+
+-- 2. Add "Euclidean PI", "The epic story of Euclid as a pizza delivery boy in      title - EUCLIDEAN PI 
+-- ancient Greece", to the film table. The movie was released in 2008 in English.   description - The epic story of Euclid as a pizza delivery boy in ancient Greece
+-- Since its an epic, the run length is 3hrs and 18mins. There are no special       release_year 2008            
+-- features, the film speaks for itself, and doesn't need any gimmicks.             length -198
+SELECT *
+from film;
+insert into film (title, description, release_year, language_id, length)
+values('EUCLIDEAN PI', 'The epic story of Euclid as a pizza delivery boy in ancient Greece', '2008', '1', '198');
+        
+-- 3. Hampton Avenue plays Euclid, while Lisa Byway plays his slightly  |Hampton and Lisa both need to be added to the film actor table
+-- overprotective mother, in the film, "Euclidean PI". Add them to the film. |to add them we need to use the actor_id & film_id values located in the film_actor table
+SELECT *
+from film_actor;
+
+insert into film_actor
+  (actor_id, film_id)
+  values ('227', '1001')
+    ;
+   insert into film_actor
+  (actor_id, film_id)
+  values ('228','1001')
+  ;
+
+-- 4. Add Mathmagical to the category table. 
+SELECT *
+from category
+;
+insert into category(name)
+values ('Mathmagical')
+;
 
 -- 5. Assign the Mathmagical category to the following films, "Euclidean PI",
 -- "EGG IGBY", "KARATE MOON", "RANDOM GO", and "YOUNG LANGUAGE"
+-- to add something to the film category table it must have the film_id & the category_id
+-- the category id for Mathmagical is 17 so all the movies listed about much have 17 in that column
+
+SELECT *
+from film_category
+;
+
+insert into film_category( film_id, category_id)
+values ('1001', '17')
+;
+update film_category
+set category_id = '17'
+where film_id = '274'
+;
+update film_category
+set category_id = '17'
+where film_id = '494'
+;
+update film_category
+set category_id = '17'
+where film_id = '714'
+;
+update film_category
+set category_id = '17'
+where film_id = '996'
+;
 
 -- 6. Mathmagical films always have a "G" rating, adjust all Mathmagical films
 -- accordingly.
 -- (5 rows affected)
+select *
+from film
+;
+update film
+set rating = 'G'
+where title in ('Euclidian PI', 'EGG IGB', 'KARATE MOON', 'RANDOM GO', 'YOUNG LANGUAGE')
+;
+
+
+
+
 
 -- 7. Add a copy of "Euclidean PI" to all the stores.
 
