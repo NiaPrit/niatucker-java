46bca0a6c9a0594c6700e601db9b43bf85e3640c
Nia Tucker <ntucker1@kent.edu>
2021-06-24 23:03:49 -0400

    13APISupdate

--- module-2/13_ServerSide_APIs_Part_1/exercise-student/java/src/main/java/com/techelevator/auctions/controller/AuctionController.java
+++ module-2/13_ServerSide_APIs_Part_1/exercise-student/java/src/main/java/com/techelevator/auctions/controller/AuctionController.java
@@ -6,24 +6,39 @@ import com.techelevator.auctions.model.Auction;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
-@RestController
-
 //@requestmapping B4 the Controller class
 //set the default base path for all paths specified in other @requestmapping
+@RestController
 @RequestMapping("/auctions")
+
 public class AuctionController {
 
-    private AuctionDAO dao;
+    private MemoryAuctionDAO dao;
 
     public AuctionController() {
 
         this.dao = new MemoryAuctionDAO();
     }
-//controller  method here
+//controller  method here step 2
     @RequestMapping(path = "", method = RequestMethod.GET)
     public List<Auction> list() {
         System.out.println("List of Auctions");
         return dao.list();
     }
-}
+
+    //step 3
+    @RequestMapping(path = "/{id}", method = RequestMethod.GET)
+    public Auction get(@PathVariable int id) {
+       //System.out.println("/auction/" + id + "path received from server");
+       return dao.get(id);
+    }
+
+    //part 4
+    @RequestMapping(path = "", method = RequestMethod.POST)
+    public Auction addAAuction(@RequestBody Auction aAuction) {
+        Auction theAuction = dao.create(aAuction);
+        return theAuction;
+    }
+
+    //part 5
+} //end of class
