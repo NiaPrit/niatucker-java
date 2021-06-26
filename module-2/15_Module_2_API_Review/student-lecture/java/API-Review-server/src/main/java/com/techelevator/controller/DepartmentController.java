f3533156f7795ff2f757b8276611713158670d73
Nia Tucker <ntucker1@kent.edu>
2021-06-26 16:47:43 -0400

    14Exercise

--- module-2/15_Module_2_API_Review/student-lecture/java/API-Review-server/src/main/java/com/techelevator/controller/DepartmentController.java
+++ module-2/15_Module_2_API_Review/student-lecture/java/API-Review-server/src/main/java/com/techelevator/controller/DepartmentController.java
@@ -1,18 +1,41 @@
 package com.techelevator.controller;
 
+import com.techelevator.model.Department;
+import com.techelevator.model.DepartmentDAO;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
- * Controller to authenticate users.
+ * Controller to process API requests.
  */
 
+
+@RestController // tells server there are methods in here to handle URL paths it might get
 public class DepartmentController {
-	
+
+ //Define a reference to the Department DAO so we can handle API calls to get Department data
+ DepartmentDAO theDeptData;
+
+ //constructor for the controller class to receieve DAO objects and assign to our references
+public DepartmentController(DepartmentDAO theDeptMethods){
+    theDeptData = theDeptMethods;
+}
+
+@RequestMapping(path="/department", method= RequestMethod.GET)
+public List<Department> getTheDepartments(){
+    List<Department> theDepartments = new ArrayList();
+    return theDepartments;
+}
 public void logAPICall(String message) {
 	 LocalDateTime now = LocalDateTime.now();
-     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("mm/dd/yyyy HH:mm:ss.A");
+     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss.A");
      String timeNow = now.format(formatter);
      System.out.println(timeNow + "-" + message);
  }
