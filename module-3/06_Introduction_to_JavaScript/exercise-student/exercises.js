bb2b2c064bc5c9de854bb4a434bf74bf1f8ddccc
Nia Tucker <ntucker1@kent.edu>
2021-07-12 23:17:40 -0400

    introJS

--- module-3/06_Introduction_to_JavaScript/exercise-student/exercises.js
+++ module-3/06_Introduction_to_JavaScript/exercise-student/exercises.js
@@ -5,13 +5,19 @@
 		sumDouble(1, 2) → 3
 		sumDouble(3, 2) → 5
 		sumDouble(2, 2) → 8
-
-		function sumDouble(x, y) {
-			// do logic here
-			// return result;
-			return x + y;
-        }
 */
+		function sumDouble(x, y) {
+			let result = 0; // define a variable to hold return value
+
+			result = x + y; // sum the two numbers
+
+			if (x===y){	//if the values are same - double result
+			result = result * 2;
+			}
+
+			return result; // return the result
+        }
+
 
 /*
 2. **hasTeen** We'll say that a number is "teen" if it is in the range 13..19 inclusive. 
@@ -21,7 +27,16 @@
 		hasTeen(20, 19, 10) → true
 		hasTeen(20, 10, 13) → true
 */
-
+		function hasTeen(x,y,z){
+		let numbers = [x,y,z];
+		let result = false;
+		for(let i=0; i < numbers.length; i++){
+			if(numbers[i] < 20 && numbers [i] >12){
+				result = true;
+			}
+		}
+		return result;
+	}
 /* 
 3. **lastDigit** Given two non-negative int values, return true if they have the same 
     last digit, such as with 27 and 57.
@@ -30,6 +45,13 @@
 		lastDigit(6, 17) → false
 		lastDigit(3, 113) → true
 */
+	function lastDigit(x,y){
+			let numbers = [x,y];
+			let results =  false;
+		return (x % 10 == y % 10);
+	}
+
+
 
 /*
 4. **seeColor** Given a string, if the string begins with "red" or "blue" return that color 
@@ -40,13 +62,15 @@
         seeColor("blueTimes") → "blue"
 */
 
+
 /*
 5. **oddOnly** Write a function that given an array of integer of any length, filters out 
     the even number, and returns a new array of just the the odd numbers.
 
 		oddOnly([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]) → [1, 3, 5, 7, 9, 11];
-		oddOnly([2, 4, 8, 32, 256]); → []
+		oddOnly([2, 4, 8, 32, 256]); → []	
 */
+
 
 /*
 6. **frontAgain** Given a string, return true if the first 2 chars in the string also appear 
@@ -116,3 +140,11 @@ firstName, lastName, and age. Populate the properties with your values.
 		age
 	}
 */
+function createObject() {
+	const obj = {
+	  firstName: "Nia",
+	  lastName: "Tucker",
+	  age: 28
+	};
+	return obj;
+  }
