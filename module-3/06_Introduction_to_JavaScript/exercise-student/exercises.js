c23aa9b747f6b74ddbd5218d60d5146dce564407
Nia Tucker <ntucker1@kent.edu>
2021-07-14 09:32:06 -0400

    update

--- module-3/06_Introduction_to_JavaScript/exercise-student/exercises.js
+++ module-3/06_Introduction_to_JavaScript/exercise-student/exercises.js
@@ -88,6 +88,8 @@ function seeColor(string){
 	}
 return result;
 }
+
+
 /*
 6. **frontAgain** Given a string, return true if the first 2 chars in the string also appear 
     at the end of the string, such as with "edited".
